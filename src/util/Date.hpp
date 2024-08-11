b7a08bf390268388ec6ed34b4b0fa33312d1a500
Xorrad <monsieurs.aymeric@gmail.com>
2024-08-11 18:07:12 +0200

    feat: add string constructor for dates

--- src/util/Date.hpp
+++ src/util/Date.hpp
@@ -7,6 +7,15 @@ struct Date {
     int month;
     int day;
 
+    Date(int y, int m, int d) : year(y), month(m), day(d) {}
+
+    Date(const std::string& str) {
+        std::vector<std::string> result = String::Split(str, ".");
+        year = std::stoi(result[0]);
+        month = std::stoi(result[1]);
+        day = std::stoi(result[2]);
+    }
+
     operator std::string() const {
         return fmt::format("{}.{}.{}", year, month, day);
     }
