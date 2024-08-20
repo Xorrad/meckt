4a77789dfdfccab6dc0828354d9253a5df40d296
Xorrad <monsieurs.aymeric@gmail.com>
2024-08-20 15:03:17 +0200

    feat(editor): add name filter to titles tab

--- src/app/menu/tab/TitlesTab.cpp
+++ src/app/menu/tab/TitlesTab.cpp
@@ -13,6 +13,42 @@ void TitlesTab::Render() {
         return;
 
     const SharedPtr<Mod> mod = this->GetMod();
+
+    // Generate a map of whether a title is filtered by name or not.
+    static std::string filter = "";
+    static std::map<std::string, bool> filteredTitles;
+    if(ImGui::InputText("filter", &filter)) {
+        filteredTitles.clear();
+
+        // All titles below a title that passes the filter will be displayed.
+        // This is done by passing whether the liege title is filtered or not
+        // to the dejure titles.
+        // On the other hand, if the liege title does not pass the filter but a dejure
+        // title does, only the liege title and that very dejure title will be shown.
+        // Example:
+        //  filter = 'dom2'
+        //  e_empire (shown)
+        //      > k_kingdom
+        //      > k_kingdom2 (shown)
+        std::function<bool(const SharedPtr<Title>&, bool)> FilterTitles = [&](const SharedPtr<Title>& title, bool liegeFiltered) {
+            bool filtered = (title->GetName().find(filter) != std::string::npos) || liegeFiltered;
+            bool finalFiltered = filtered;
+            if(!title->Is(TitleType::BARONY)) {
+                // FilterTitles need to be first in the operation, otherwise
+                // it will not be called if finalFiltered is already true.
+                const SharedPtr<HighTitle>& highTitle = CastSharedPtr<HighTitle>(title);
+                for(const auto& dejureTitle : highTitle->GetDejureTitles())
+                    finalFiltered = FilterTitles(dejureTitle, filtered) || finalFiltered;
+            }
+            if(!filtered) filteredTitles[title->GetName()] = finalFiltered;
+            return finalFiltered;
+        };
+
+        for(const auto& [name, title] : mod->GetTitles()) {
+            if(title->GetLiegeTitle() == nullptr)
+                FilterTitles(title, false);
+        }
+    }
     
     if(ImGui::BeginTable("Titles Tree", 3, ImGuiTableFlags_Resizable)) {
         ImGui::TableSetupColumn("Name");
@@ -21,6 +57,9 @@ void TitlesTab::Render() {
         ImGui::TableHeadersRow();
 
         std::function<void(const SharedPtr<Title>&)> DisplayTitle = [&](const SharedPtr<Title>& title) {
+            if(filteredTitles.count(title->GetName()) > 0 && !filteredTitles[title->GetName()])
+                return;
+
             ImGui::TableNextRow();
             ImGui::TableNextColumn();
 
