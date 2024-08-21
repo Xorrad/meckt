d41627b1da52390bb25fe8224d482c3b71da8569
Xorrad <monsieurs.aymeric@gmail.com>
2024-08-21 12:42:45 +0200

    feat(editor): add button to switch between province and barony

--- src/app/menu/tab/PropertiesTab.cpp
+++ src/app/menu/tab/PropertiesTab.cpp
@@ -143,6 +143,14 @@ void PropertiesTab::RenderProvinces() {
                 ImGui::EndCombo();
             }
 
+            if(m_Menu->GetApp()->GetMod()->GetTitles().count(province->GetName()) > 0) {
+                if(ImGui::Button("Switch to barony")) {
+                    const SharedPtr<Title>& title = m_Menu->GetApp()->GetMod()->GetTitles()[province->GetName()];
+                    m_Menu->SwitchMapMode(MapMode::BARONY, true);
+                    m_Menu->GetSelectionHandler().Select(title);
+                }
+            }
+
             ImGui::PopID();
         }
     }
@@ -175,6 +183,14 @@ void PropertiesTab::RenderTitles() {
             if(title->Is(TitleType::BARONY)) {
                 const SharedPtr<BaronyTitle>& barony = CastSharedPtr<BaronyTitle>(title);
                 ImGui::InputInt("province id", &barony->m_ProvinceId);
+
+                if(ImGui::Button("Switch to province")) {
+                    if(m_Menu->GetApp()->GetMod()->GetProvincesByIds().count(barony->GetProvinceId()) > 0) {
+                        const SharedPtr<Province>& province = m_Menu->GetApp()->GetMod()->GetProvincesByIds()[barony->GetProvinceId()];
+                        m_Menu->SwitchMapMode(MapMode::PROVINCES, true);
+                        m_Menu->GetSelectionHandler().Select(province);
+                    }
+                }
             }
             else {
                 const SharedPtr<HighTitle>& highTitle = CastSharedPtr<HighTitle>(title);
