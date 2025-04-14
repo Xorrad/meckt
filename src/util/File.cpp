a0e48e518299817de05f9b6c8a54765aeadbb012
Xorrad <monsieurs.aymeric@gmail.com>
2025-04-14 16:25:18 +0200

    fix: make listing files recursive by default (e.g for localizations)

--- src/util/File.cpp
+++ src/util/File.cpp
@@ -1,11 +1,17 @@
 #include "File.hpp"
 #include <filesystem>
 
-std::set<std::string> File::ListFiles(const std::string& dirPath) {
+std::set<std::string> File::ListFiles(const std::string& dirPath, bool recursive) {
     std::set<std::string> files;
     if(std::filesystem::exists(dirPath)) {
-        for (const auto& entry : std::filesystem::directory_iterator(dirPath))
-            files.insert(entry.path().string());
+        if(recursive) {
+            for (const auto& entry : std::filesystem::recursive_directory_iterator(dirPath))
+                files.insert(entry.path().string());
+        }
+        else {
+            for (const auto& entry : std::filesystem::directory_iterator(dirPath))
+                files.insert(entry.path().string());
+        }
     }
     return files;
 }
