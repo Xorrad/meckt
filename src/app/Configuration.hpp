4625c813d814b20ff0c0b63a1c8c6deeb7992ef9
Xorrad <monsieurs.aymeric@gmail.com>
2026-02-01 12:44:45 +0100

    feat: change version to 1.7.3

--- src/app/Configuration.hpp
+++ src/app/Configuration.hpp
@@ -27,7 +27,7 @@ public:
     Configuration& operator=(const Configuration&) = delete;
 
     //Settings
-    inline static std::string buildVersion = "1.7.2";
+    inline static std::string buildVersion = "1.7.3";
     inline static std::string buildCredits = "made by Xorrad";
     inline static std::string githubURL = "https://github.com/Xorrad/meckt";
     inline static std::string atlantisURL = "https://codeload.github.com/bombusfrigidus/Atlantis/zip/refs/heads/main";
