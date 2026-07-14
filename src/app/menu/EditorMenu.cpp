d598dabbf942464db9f99e6de0bc8ada38018946
Xorrad <monsieurs.aymeric@gmail.com>
2026-07-14 11:02:40 +0200

    fix: numbers weren't allowed in title name in create title modal

--- src/app/menu/EditorMenu.cpp
+++ src/app/menu/EditorMenu.cpp
@@ -693,6 +693,7 @@ void EditorMenu::RenderMenuBarTools() {
 static int FilterTitleName(ImGuiInputTextCallbackData* data) { 
     ImWchar c = data->EventChar;
     if((c >= 'a' && c <= 'z') || c >= '_') return 0;
+    if((c >= '0' && c <= '9')) return 0;
     if(c >= 'A' && c <= 'Z') { data->EventChar += 'A'-'a'; return 0; }
     return 1;
 }
